bbacd93684d1ee21fe66da546a129bb47ea9b12d
Derek Knaggs <knad1_14@uni.worc.ac.uk>
2016-02-18 16:50:29 +0000

    First Commit

---
+++ cordova_plugins.js
@@ -0,0 +1,36 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
+        "id": "cordova-plugin-x-toast.Toast",
+        "clobbers": [
+            "window.plugins.toast"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-x-toast/test/tests.js",
+        "id": "cordova-plugin-x-toast.tests"
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.dialogs": "0.3.0",
+    "cordova-plugin-x-toast": "2.4.0"
+}
+// BOTTOM OF METADATA
+});
